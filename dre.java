8d54bb729d2fa5af6c5b735056dacff9812f4a09
RUCHIT680 <71620968+RUCHIT680@users.noreply.github.com>
2020-09-25 21:36:03 +0530

    Update dre.java

--- dre.java
+++ dre.java
@@ -1,17 +1,22 @@
 
-public class dre {public class UC1 {public class UC1 {
+public class dre  {
 	public static void main(string[] args) {
 		int FULL_TIME = 1;
+		emprate/hr = 20;
+		int emphrs = 0;
+		int empwage = 0;
+		
 		double empcheck = Math.floor(Math.random() * 10) % 2;
 		if (empcheck == FULL_TIME)
-			System.out.println("present");
-		else 
-			System.out.println("absent");
+			emphrs = 8;
+		else emphrs = 0;
+		empwage = emphrs * emprate/hr;
+		
+		
+			System.out.println( " empwage = +emp rate/hr ");
 		
 	}
 }
 UC1.java(end)
 
-
-}System.out.println("absent");
 
