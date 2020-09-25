5ef21ea91ba567b764d9d1dddb318a01c775c1dd
RUCHIT680 <ruchitrai282@gmail.com>
2020-09-25 21:19:43 +0530

    added UC4

--- dre.java
+++ dre.java
@@ -1,28 +1,32 @@
 
-public class dre {public class UC1 {public class empwagebuilderif {
+public class dre {public class UC4 {public class empwagebuildercase {
+	public static final int PART_TIME = 1;
+	public static final int FULL_TIME = 2;
+	public static final int emprate/hr = 20;
 	public static void main(string[] args) {
+		int emphrs = 0;
+	}   int empwage = 0;
 		int PART_TIME = 1;
 		int FULL_TIME = 2;
 		int emprate/hr = 20;
 		int emphrs = 0;
-		int empwage = 0;
 		
-		double empcheck = Math.floor(Math.random() * 10) % 3;
-		if (empcheck == PART_TIME)
+		
+		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empcheck) {
+		case PART_TIME:
 			emphrs = 4;
-		else if (empcheck == FULL_TIME)
+			break;
+		case PART_TIME:
 			emphrs = 8;
-		else
-			emphrs = 0;
-		empwage = emphrs * emprate/hr;
-		System.out.println("emp wage:" + empwage);
-			
-					
-		
-	}
+			break;
+			default:
+				emphrs = 0;
+		}empwage = emphrs * emprate/hr;
+		system.out.println("emp wage :" + empwage);
 }
-UC3.java(end)
+}
+		 uc4. end
+		
 
-
-}System.out.println("absent");
 
