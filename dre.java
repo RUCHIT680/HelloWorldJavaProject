502ecd138d7f6ad5e034ece89efc9bd39f941cc7
RUCHIT680 <ruchitrai282@gmail.com>
2020-09-25 20:42:18 +0530

    added UC3

--- dre.java
+++ dre.java
@@ -1,16 +1,27 @@
 
-public class dre {public class UC1 {public class UC1 {
+public class dre {public class UC1 {public class empwagebuilderif {
 	public static void main(string[] args) {
-		int FULL_TIME = 1;
-		double empcheck = Math.floor(Math.random() * 10) % 2;
-		if (empcheck == FULL_TIME)
-			System.out.println("present");
-		else 
-			System.out.println("absent");
+		int PART_TIME = 1;
+		int FULL_TIME = 2;
+		int emprate/hr = 20;
+		int emphrs = 0;
+		int empwage = 0;
+		
+		double empcheck = Math.floor(Math.random() * 10) % 3;
+		if (empcheck == PART_TIME)
+			emphrs = 4;
+		else if (empcheck == FULL_TIME)
+			emphrs = 8;
+		else
+			emphrs = 0;
+		empwage = emphrs * emprate/hr;
+		System.out.println("emp wage:" + empwage);
+			
+					
 		
 	}
 }
-UC1.java(end)
+UC3.java(end)
 
 
 }System.out.println("absent");
