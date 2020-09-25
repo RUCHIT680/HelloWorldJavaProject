1acfc4da6c0040199832a0dddfe493676d85e85b
RUCHIT680 <ruchitrai282@gmail.com>
2020-09-25 21:47:59 +0530

    added UC5

--- dre.java
+++ dre.java
@@ -1,16 +1,16 @@
 
-public class dre {public class UC4 {public class empwagebuildercase {
+public class dre {
 	public static final int PART_TIME = 1;
 	public static final int FULL_TIME = 2;
 	public static final int emprate/hr = 20;
+	public static final int no_of_working_days = 2;
+	
 	public static void main(string[] args) {
 		int emphrs = 0;
-	}   int empwage = 0;
-		int PART_TIME = 1;
-		int FULL_TIME = 2;
-		int emprate/hr = 20;
-		int emphrs = 0;
-		
+	   int empwage = 0;
+	total empwage = 0;
+	for (int day = 0; day < no_of_working_days; day++)
+	{ 
 		
 		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch (empcheck) {
@@ -22,11 +22,17 @@ public class dre {public class UC4 {public class empwagebuildercase {
 			break;
 			default:
 				emphrs = 0;
-		}empwage = emphrs * emprate/hr;
+		}
+		empwage = emphrs * emprate/hr;
+				total empwage += empwage;
+		
+		
 		system.out.println("emp wage :" + empwage);
 }
-}
-		 uc4. end
+	system.out.println("empwage: " + empwage);
+	}
+		
+}uc5. end
 		
 
 
