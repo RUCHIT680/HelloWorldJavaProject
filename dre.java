eaae956c1d36c34b0cdaccd2003259dba1c0e3b5
RUCHIT680 <71620968+RUCHIT680@users.noreply.github.com>
2020-10-08 01:10:53 +0530

    Update dre.java

--- dre.java
+++ dre.java
@@ -1,26 +1,12 @@
 
-public class dre {public class UC1 {public class UC2 {
-	public static void main(string[] args) {
-		int FULL_TIME = 1;
-		int emp_rate/hr = 20;
-		int emphrs = 0;
-		int empwage = 0;
-		
-		double empcheck = Math.floor(Math.random() * 10) % 2;
-		if (empcheck == FULL_TIME)
-			emphrs = 8;
-		else
-			emphrs = 0;
-		empwage = emphrs * emp_rate/hr;
-		
-			
-			System.out.println("empwage: +empwage");
-	
-		
-	}
-}
-UC2.java(end)
+package EmployeeWageCode;
 
+public class EmployeeWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+}
 
 
 
